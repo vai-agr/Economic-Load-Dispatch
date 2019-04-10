43f0903927214261a2b4d6503d1d8429dfaa86b1
Vaibhav Agrawal <vaiagr2011@gmail.com>
2019-04-10 20:23:38 +0530

    lam iter 3

--- eld_li.m
+++ eld_li.m
@@ -1,5 +1,6 @@
 clc;
 Pd = 850;
+a= [561 310 78];
 b = [7.92 7.85 7.97];
 c = [0.001562 0.00194 0.00482];
 pl = [150 100 50];
@@ -8,12 +9,19 @@ iter =0;
 x = max(b);
 dp = Pd;
 alph = min(c)
+c_list =[];
 
 while(abs(dp)> 0.0001)
-	P = (x-b)/(2*c);
+	P = (x-b)./(2*c);
 	P = min(P,ph);
 	P = max(P,pl);
 	dp = Pd - sum(P);
 	x = x+dp*alph;
 	iter+=1;
-	disp(iter, x);
+	C = sum(a+b.*P+c.*P.*P);
+	c_list = [c_list,C];
+	disp(iter);
+	disp(x);
+	disp(C);
+end
+plot(1:iter, c_list);
